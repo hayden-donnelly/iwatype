9fdba5fbe7b09605f907dca8b0cac5fe7139e3fa
hayden-donnelly <donnellyhd@outlook.com>
2022-06-14 16:19:56 -0400

    broke everything. Gotta rewrite with Express.

--- app.js
+++ app.js
@@ -2,17 +2,27 @@ const http = require ("http");
 const fs = require("fs");
 const port = 3000;
 
+const quotes = ["Don’t walk in front of me… I may not follow. Don’t walk behind me… I may not lead. Walk beside me… just be my friend",
+                "Without music, life would be a mistake.",
+                "Man is abandonned upon the earth, condemned to be free."]
+
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {"Content-Type": "text/html"});
-    fs.readFile("index.html", (err, dat) => {
-        if(err) {
-            res.writeHead(404);
-            res.write("Error: File Not Found");
-        } else {
-            res.write(dat);
-        }
+    if(req.url == "new-quote") {
+        res.writeHead(200, {"Content-Type": "text/txt"});
+        res.write(quotes[parseInt(Math.random() * (quotes.length-1), 10)])
         res.end();
-    });
+    } else {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        fs.readFile("index.html", (err, dat) => {
+            if(err) {
+                res.writeHead(404);
+                res.write("Error: File Not Found");
+            } else {
+                res.write(dat);
+            }
+            res.end();
+        });
+    }
 });
 
 server.listen(port);
