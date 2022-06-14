58b479366c5888b2d5f2dd851c0ca3a1b156f7cb
hayden-donnelly <donnellyhd@outlook.com>
2022-06-14 19:45:29 -0400

    ..

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const { readFile } = require("fs").promises;
 const port = 3000;
 const app = express();
 
-const quotes = ["Don’t walk in front of me… I may not follow. Don’t walk behind me… I may not lead. Walk beside me… just be my friend",
+const quotes = ["Don't walk in front of me... I may not follow. Don't walk behind me... I may not lead. Walk beside me... just be my friend.",
                 "Without music, life would be a mistake.",
                 "Man is abandonned upon the earth, condemned to be free."]
 
@@ -12,7 +12,6 @@ app.get("/", async (req, res) => {
 });
 
 app.get("/new-quote", async (req, res) => {
-    console.log(parseInt(Math.random() * quotes.length, 10));
     res.send(quotes[parseInt(Math.random() * quotes.length, 10)]);
 });
 
